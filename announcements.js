2940b6722a72a59a316154efb4ed95a2f0b6fb2b
bathoneapsychology <bathoneasnal@gmail.com>
2026-02-24 21:42:46 +0300

    Update announcements.js

--- announcements.js
+++ announcements.js
@@ -1,15 +1,24 @@
 const announcements = [
   {
+    id: 1,
+    title: "Yeni Workshop Duyurusu",
+    date: "2026-03-10",
+    text: "Psikoloji kulübümüzün düzenleyeceği yeni workshop çok yakında. Katılım için takipte kalın!",
+    img: "duyurular-images/etkinlik2.jpg"
+  },
+  {
+    id: 2,
     title: "Çok Yakında Aranızdayız!",
     date: "2026-02-23",
     text: "Avcılar Süleyman Nazif Anadolu Lisesi öğrencilerinden oluşan Bathonea Psikoloji kulübü çok yakında sizlerle! Öğrencilerin kaleme aldığı yazılar, bizzat seslendikleri podcast'ler ve daha fazlası... Takipte kalın!",
     img: "duyurular-images/etkinlik1.jpg"
   },
   {
-    title: "",
-    date: "",
-    text: "",
-    img: ""
+    id: 3,
+    title: "Podcast Serimiz Başlıyor!",
+    date: "2026-02-15",
+    text: "Bathonea Sohbetleri podcast serimiz yayında! İlk bölümde öğrencilerimizin gözünden psikoloji dünyasını keşfedin.",
+    img: "duyurular-images/podcast1.jpg"
   }
 ];
 
