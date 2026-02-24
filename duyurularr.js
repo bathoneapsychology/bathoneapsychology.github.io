fbd9caeff35362d5f90e68a8b2dd516d190c992b
bathoneapsychology <bathoneasnal@gmail.com>
2026-02-24 20:15:34 +0300

    Create duyurularr.js

---
+++ duyurularr.js
@@ -0,0 +1,25 @@
+const announcements = [
+{
+id: 1,
+title: "Yeni Kulüp Toplantısı",
+date: "2026-03-01",
+content: "Toplantı detayları burada.",
+image: "images/toplanti.jpg"
+},
+{
+id: 2,
+title: "Workshop Duyurusu",
+date: "2026-02-25",
+content: "Workshop açıklaması.",
+image: "images/workshop.jpg"
+},
+{
+id: 3,
+title: "Seminer",
+date: "2026-02-20",
+content: "Seminer detayları.",
+image: "images/seminer.jpg"
+}
+];
+
+window.announcements = announcements;
