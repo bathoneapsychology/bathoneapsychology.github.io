03636902dbe7cda4db83164e4be62f8bc45ea992
bathoneapsychology <bathoneasnal@gmail.com>
2026-02-25 06:26:26 +0300

    Update announcements.js

--- announcements.js
+++ announcements.js
@@ -4,7 +4,7 @@ const announcements = [
     title: "Çok Yakında Aranızdayız!",
     date: "2026-02-23",
     text: "Avcılar Süleyman Nazif Anadolu Lisesi öğrencilerinden oluşan Bathonea Psikoloji kulübü çok yakında sizlerle! Öğrencilerin kaleme aldığı yazılar, bizzat seslendirdikleri podcast'ler ve daha fazlası... Takipte kalın!",
-    img: "duyurular-images/etkinlik1.jpg"
+    img: "images/snalokul.jpg" , "images/snallogo.jpg"
   },
   {
     id: 2,
